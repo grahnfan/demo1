74661412065f0611be250cac833138256f889ab7
Erik <Erik@10.37.129.2>
2016-08-20 12:53:01 +0200

    added Hej Erik

---
+++ src/demo/Demo.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2016
+ */
+package demo;
+
+/**
+ *
+ * @author Erik
+ */
+public class Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hej Erik");
+    }
+    
+}
